8a04c63e82483d2b3613d2fc17e78401c3b2e52c
bellicapelli0 <orlandoscarpa95@gmail.com>
2021-10-04 19:27:55 +0200

    api fix & README update

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # FRISS-technical-assignment
-Technical assignment for FRISS 
+Technical assignment for FRISS
+
+### Docker API instructions
+
+to build and start the container run the following commands: 
+```
+sudo docker build -t friss-api .
+sudo docker run -d -p 5000:5000 friss-api
+```
+
+The "score" endpoint takes a claim ID as argument and returns the predicted class for the relative claim.
+Example:
+
+```
+curl -XGET 0.0.0.0:5000/score/140152636
+```
+
+The "predict" endpoint predicts the class using the features passed through a json file and saves the predicted class, so it can be reaccessed through the "score" endpoint
+
+```
+curl -H "Content-Type: application/json" -d @test.json 0.0.0.0:5000/predict/
+```
--- api.py
+++ api.py
@@ -15,6 +15,7 @@ NUMBER_OF_FEATURES = 23
 app = Flask(__name__)
 
 df = pd.read_csv(PREDICTION_FILE)
+print(df)
 df["ID"] = df["ID"].astype(str)
 
 model = ClaimClassifier(NUMBER_OF_FEATURES)
@@ -28,25 +29,33 @@ def score(Line):
     sub_df = df[df["ID"] == Line]["prediction"]
     if len(sub_df) == 1:
         score = sub_df[0]
-        response = {"score":score}
+        response = {"prediction":score}
         return response
     else:
         return "could not find claimid\n"
     
 @app.route('/predict/', methods=["POST"])
 def predict():
-    content = request.json
-    ID = content["id"]
-    x = torch.tensor(content["features"])
-    
-    with torch.no_grad():
-        logits = model(x)
-    probs = softmax(logits).detach().cpu().numpy()
-    pred = int(np.argmax(logits))
+    try:
+        content = request.json
+        ID = content["id"]
+        x = torch.tensor(content["features"])
+
+        with torch.no_grad():
+            logits = model(x)
+
+        probs = softmax(logits).detach().cpu().numpy()
+        pred = int(np.argmax(logits))
+    except (RuntimeError, ValueError, KeyError) as e:
+            return "There was an error while predicting the claim, please check the input features.\n"
 
     r = { "prediction" : pred,
          "score" : float(probs[pred])
         }
+      
+    global df
+    df = df.append({"ID":ID, "prediction":float(pred)}, ignore_index=True)
+    df.to_csv("test.csv", index=False)
     
     return r
     
